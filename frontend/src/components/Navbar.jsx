4aa20c7b8189838d4aebfe1ecdcf5f4050644c79
Rohit Singh <rohitsrcoder@gmail.com>
2024-11-10 16:32:36 +0530

    Update Navbar.jsx
    logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -13,7 +13,9 @@ const Navbar = () => {
 		<header className='fixed top-0 left-0 w-full bg-gray-900 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-emerald-800'>
 			<div className='container mx-auto px-4 py-3'>
 				<div className='flex flex-wrap justify-between items-center'>
+					
 					<Link to='/' className='text-2xl font-bold text-emerald-400 items-center space-x-2 flex'>
+						 <img src="./fa67eefbfc3e35b9b721b5e8ecdcbee5.jpg" alt='logo' className='w-9 h-9 bg-red-50 object-contain rounded-full' />
 						Loosely Fit
 					</Link>
 
